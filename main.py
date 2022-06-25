cbff38d2b0b08ad773739437be7d6123ac139dd6
Moinam Chatterjee <moinamc2007@yahoo.co.in>
2022-06-26 00:00:28 +0200

    enable CORS

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template, request, jsonify
+from flask_cors import CORS
 from backend.helpers import bigquery
 app = Flask(__name__)
-
+CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 @app.route("/")
 def main():
